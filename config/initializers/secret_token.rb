70e11ae05165fb4021a3d06f74331a259241f99f
Poorva Mahajan <poorva.mahajan2990@gmail.com>
2015-08-15 18:48:04 +0530

    Initialised karyalay repo

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Karyalay::Application.config.secret_key_base = '319ba084d4946acc5b4bf2dd9736a430d0354c53d64723e82e30822f513c3ab9e276ca06b8312cd323c5c650d8db53644c5e8ecf61803353cd23777e9a3d67ab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Karyalay::Application.config.session_store :cookie_store, key: '_karyalay_session'
